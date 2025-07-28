ce607d1f34addf9e4e5418bc884efdc71abc1a6e
Gloriana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-07-28 13:39:51 -0600

    Clase luz Intermitente
    Se crea la clase luz intermitente, con su propio metodo, ademas implemente la interrfaz encender, extiende de luces. Y contiene su propio atributo booleano, getter y constructor

---
+++ src/sistIluminacion/newpackage/LuzIntermitente.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistIluminacion.newpackage;
+
+/**
+ *
+ * @author Gloriana
+ */
+public abstract class LuzIntermitente extends Luces {
+//volatible se usa para que los hilos esten atentos al valor de este boleano ya que sera de control
+    private volatile boolean luzIntermitente;
+    
+    //porque?
+    public LuzIntermitente(boolean luz) {
+        super(luz);
+    }
+
+    public boolean isLuzIntermitente() {
+        return luzIntermitente;
+    }
+    public void Luz_intermitente() {
+        this.luzIntermitente = false;
+}
+    @Override
+    public void encender(boolean luzIntermitente){
+    this.luzIntermitente = true;
+}   
+    public void apagar(boolean luzIntermitente){
+    this.luzIntermitente = true;
+} 
+    public void funcionIntermitente(boolean luzIntermitente){
+        while(true){
+            if (!luzIntermitente){ 
+                try{
+                    Thread.sleep(5000);
+                    
+                    if(luzIntermitente = false){
+                        
+                     break;  
+                     
+                    }}catch(InterruptedException e){
+                    Thread.currentThread().interrupt();// interrumpe el hilo y lo restaura para volver a usarlo
+                    
+                    luzIntermitente = false; 
+                }  
+            } 
+        } 
+    }
+}
