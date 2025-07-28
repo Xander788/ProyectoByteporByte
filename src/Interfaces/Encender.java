be8a1c3f108c393ff01b0f0c46988c6ce017d1f0
Gloriana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-07-28 13:30:13 -0600

    Clase Encender y paquete Interfaces
    Se crea la clase interface encender que contiene dos metodos encender y apagar

---
+++ src/Interfaces/Encender.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Interfaces;
+
+/**
+ *
+ * @author Gloriana
+ */
+public interface Encender {
+    public void encender();
+    public void apagar();   
+}
