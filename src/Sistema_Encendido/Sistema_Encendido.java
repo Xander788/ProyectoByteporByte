d1f29167985ea5bab9ed3122071782e0a7db7648
Brayan <brazofeifaam@est.utn.ac.cr>
2025-07-28 19:21:14 -0600

    Correccion de metodo Constructor
    Se corrigió el método constructor de la clase sistema encendido

--- src/Sistema_Encendido/Sistema_Encendido.java
+++ src/Sistema_Encendido/Sistema_Encendido.java
@@ -4,10 +4,34 @@
  */
 package Sistema_Encendido;
 
+import Interfaces.Encender;
+
 /**
  *
  * @author Braya
  */
-public class Sistema_Encendido {
+public class Sistema_Encendido implements Encender{
+     private boolean encendido; // Estado del sistema (true = encendido, false = apagado)
+
+    // Método que devuelve el estado actual del sistema
+    public boolean isEncendido() {
+        return encendido;
+    }
+
+    // Constructor: inicializa el sistema como apagado
+    public Sistema_Encendido() {
+        this.encendido = false; // inicia apagado
+    }
     
+    @Override
+    public void encender() {
+        if(!this.encendido){
+            this.encendido=true;
+        }
+    }
+        
+    @Override
+    public void apagar() {
+       this.encendido=false;
+    } 
 }
