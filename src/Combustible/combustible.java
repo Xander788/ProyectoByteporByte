54dbccceff10f6cfccbbaa734422f3e407fc80b9
oscar <osalvarezcr@est.utn.ac.cr>
2025-07-28 21:32:52 -0600

    paquete combustible
    se crea elpaquete de combustible con sus metodos nesesarios

---
+++ src/Combustible/combustible.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Combustible;
+
+/**
+ *
+ * @author oscar
+ */
+public class combustible {
+    private double nivel;
+
+    public double getNivel() {
+        return nivel;
+    }
+    
+    public double consumo(double nivel){
+        return nivel; 
+    }  
+
+    public combustible(double nivel) {
+        this.nivel = 100.0;
+    }
+}
