e9a2cbe801647acdd13fbd0a532e29d202e83fc1
Gloriana Carrillo Alfaro <glorianacarrilloalfaro@gmail.com>
2025-07-29 16:10:51 -0600

    Clase Parabrisas (modificacion)
    Se modifico la clase ya que tenia dos booleanos en y la logica no se comprendia

--- src/sistParabrisas/Parabrisas.java
+++ src/sistParabrisas/Parabrisas.java
@@ -9,31 +9,18 @@ import Interfaces.Encender;
  *
  * @author Gloriana
  */
-public abstract class Parabrisas implements Encender {
-   private boolean actividadParabrisas;
-   private String velocidadParabrisas;
+public abstract class Parabrisas {
+   private EnumParabrisas actividadParabrisas;
 
-    public boolean isActividadParabrisas() {
+    public EnumParabrisas isActividadParabrisas() {
         return actividadParabrisas;
     }
-
-    public String getVelocidadParabrisas() {
-        return velocidadParabrisas;
-    }
     //Necesario para poder usar el enum
-    public void setVelocidadParabrisas(String velocidadParabrisas) {
-        this.velocidadParabrisas = velocidadParabrisas;
-    }    
-    //constructor
-    public Parabrisas(boolean actividadParabrisas, String velocidadParabrisas) {
-        this.actividadParabrisas = false;
-        this.velocidadParabrisas = "MIN";
+    public void setActividadParabrisas(EnumParabrisas actividadParabrisas) {       
+        this.actividadParabrisas = actividadParabrisas;
     }
-    
-    public void encender(boolean actividadParabrisas){
-    this.actividadParabrisas = true;
-    } 
-    public void apagar(boolean actividadParabrisas){
-    this.actividadParabrisas = false;
-    } 
+    //constructor
+    public Parabrisas() {
+        this.actividadParabrisas = EnumParabrisas.APAGADO;
+    }
 }
