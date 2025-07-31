8f5bfad6b9df904d3d421f9928654de16c075e3b
oscar <osalvarezcr@est.utn.ac.cr>
2025-07-31 10:48:01 -0600

    modificaciones en destok
    se implementa la el internal frame al destok para su ejecucio

--- src/GUI/Desktop.java
+++ src/GUI/Desktop.java
@@ -33,6 +33,7 @@ public class Desktop extends javax.swing.JFrame {
         jDesktopPane1 = new javax.swing.JDesktopPane();
         btnMotor = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
+        btnRadio = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -50,8 +51,16 @@ public class Desktop extends javax.swing.JFrame {
             }
         });
 
+        btnRadio.setText("Radio/A/C");
+        btnRadio.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRadioActionPerformed(evt);
+            }
+        });
+
         jDesktopPane1.setLayer(btnMotor, javax.swing.JLayeredPane.DEFAULT_LAYER);
         jDesktopPane1.setLayer(jButton1, javax.swing.JLayeredPane.DEFAULT_LAYER);
+        jDesktopPane1.setLayer(btnRadio, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
         javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
         jDesktopPane1.setLayout(jDesktopPane1Layout);
@@ -62,7 +71,9 @@ public class Desktop extends javax.swing.JFrame {
                 .addComponent(btnMotor, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(614, Short.MAX_VALUE))
+                .addGap(63, 63, 63)
+                .addComponent(btnRadio, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(454, Short.MAX_VALUE))
         );
         jDesktopPane1Layout.setVerticalGroup(
             jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -70,7 +81,8 @@ public class Desktop extends javax.swing.JFrame {
                 .addGap(21, 21, 21)
                 .addGroup(jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnMotor, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnRadio, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(444, Short.MAX_VALUE))
         );
 
@@ -101,6 +113,15 @@ public class Desktop extends javax.swing.JFrame {
         sistema.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void btnRadioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRadioActionPerformed
+        Radio_AC_CAFrm radio = new Radio_AC_CAFrm();
+        Radio_AC_CAFrm climatizacion = new Radio_AC_CAFrm();
+        jDesktopPane1.add(radio);
+        jDesktopPane1.add(climatizacion);
+        radio.setVisible(true);  
+        climatizacion.setVisible(true);
+    }//GEN-LAST:event_btnRadioActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -121,13 +142,14 @@ public class Desktop extends javax.swing.JFrame {
             logger.log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(() -> new Desktop().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnMotor;
+    private javax.swing.JButton btnRadio;
     private javax.swing.JButton jButton1;
     private javax.swing.JDesktopPane jDesktopPane1;
     // End of variables declaration//GEN-END:variables
